f08474d24dd4819693accfd2f64fbe0f348dd228
luka2220 <piplicaluka64@gmail.com>
2021-12-22 17:03:03 -0500

    Board created, implementing main game loop

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import numpy as np
+
+
+# will create the game board using a matrix
+def create_board():
+    # creates a 6x7 board with all values being 0
+    board = np.zeros((6, 7))
+    return board
+
+
+# main game loop
+board = create_board()  # initializing an empty board
+game_over = False   # will switch to True if a player connected four
+turn = 0    # used to identify which players turn it is (0 = player1, 1 = player2)
+
+while not game_over:
+    # Ask for player 1 input
+    if turn == 0:
+        selection = int(input("Player 1 Make your Selection (0-6):"))
+
+    # Ask for player 2 input
+    else:
+        selection = int(input("Player 2 Make your Selection (0-6):"))
 
