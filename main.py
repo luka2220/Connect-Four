a67978dc368510c4ee39554864660d1cb60ea748
luka2220 <piplicaluka64@gmail.com>
2021-12-23 14:05:21 -0500

    Update main.py
    Implemented method to check wether player can drop a piece in specific column

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ def drop_piece():
     pass
 
 
-def is_valid_location():
+def is_valid_location(board, col):
+    return board[5][col] == 0
+
+
+def get_next_open_rows():
     pass
 
 
@@ -20,15 +24,16 @@ def is_valid_location():
 board = create_board()  # initializing an empty board
 game_over = False   # will switch to True if a player connected four
 turn = 0    # used to identify which players turn it is (0 = player1, 1 = player2)
+print(board)
 
 while not game_over:
     # Ask for player 1 input
     if turn == 0:
-        selection = int(input("Player 1 Make your Selection (0-6):"))
+        col = int(input("Player 1 Make your Selection (0-6):"))
 
     # Ask for player 2 input
     else:
-        selection = int(input("Player 2 Make your Selection (0-6):"))
+        col = int(input("Player 2 Make your Selection (0-6):"))
 
     turn += 1
     turn = turn % 2
